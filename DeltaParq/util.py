c37d0e876960ee3739d535f2479449867f721ab4
h0psej0ch <t_bottenberg@outlook.com>
2026-06-13 23:08:03 +0200

    Initial Paper writing commit

--- DeltaParq/util.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession, DataFrame
-
-def decompose(dataframe, schema_config: dict):
-    volatile_cols = schema_config["volatiles"] + schema_config["primary_key"]
-    volatile = dataframe.select(volatile_cols).dropDuplicates(volatile_cols)
-    non_volatile_cols = [x for x in dataframe.columns if x not in schema_config["volatiles"]]
-    non_volatile = dataframe.select(non_volatile_cols)
-    return (volatile, non_volatile)
